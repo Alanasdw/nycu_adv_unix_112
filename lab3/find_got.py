a3b1f42af6c53bc653c68e4228609ff9b928849a
Alanasdw <alanasdw@gmail.com>
2024-03-27 05:45:04 +0000

    finished lab3 and succesfully confirmed by the server

--- lab3/find_got.py
+++ lab3/find_got.py
@@ -1,9 +1,19 @@
 from pwn import *
-elf = ELF('./maze')
+elf = ELF('./lab03_dist/maze')
 print("main =", hex(elf.symbols['main']))
 print("{:<12s} {:<10s} {:<10s} {:<10s}".format("Func", "GOT Offset", "Symbol Offset", "Location"))
+offsets = []
 # fixed the range from 1200
 for s in [ f"move_{i}" for i in range(1201)]:
    if s in elf.got:
-      loc = elf.symbols['main'] - elf.got[s] + elf.symbols[s]
-      print("{:<12s} {:<10x} {:<13x} {:<10s}".format(s, elf.got[s], elf.symbols[s], hex(loc)))
+      loc = elf.got[s] - elf.symbols['main']
+      print("{:<12s} {:<10x} {:<13x} {:<10s}".format(s, elf.got[s], elf.symbols[s], hex( loc)))
+      offsets.append( hex( loc))
+
+print("\tint move_target[] = { ", end="")
+for i in range( len( offsets)):
+   pre = ", "
+   if ( i == 0):
+      pre = ""
+   print( pre + offsets[ i], end="")
+print("};")
