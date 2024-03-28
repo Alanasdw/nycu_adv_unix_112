695f1bff153a27ecb0a710bc1639166ec490ebaf
Alanasdw <alanasdw@gmail.com>
2024-03-28 05:10:19 +0000

    fixing bug on the number of steps, matching it with the real steps instead of leaving one with uninitialized value adding suport to quickly switch between the local and remote version

--- lab3/find_got.py
+++ lab3/find_got.py
@@ -17,3 +17,23 @@ for i in range( len( offsets)):
       pre = ""
    print( pre + offsets[ i], end="")
 print("};")
+
+# for local version
+elf = ELF('./maze')
+print("main =", hex(elf.symbols['main']))
+# print("{:<12s} {:<10s} {:<10s} {:<10s}".format("Func", "GOT Offset", "Symbol Offset", "Location"))
+offsets = []
+# fixed the range from 1200
+for s in [ f"move_{i}" for i in range(1201)]:
+   if s in elf.got:
+      loc = elf.got[s] - elf.symbols['main']
+      # print("{:<12s} {:<10x} {:<13x} {:<10s}".format(s, elf.got[s], elf.symbols[s], hex( loc)))
+      offsets.append( hex( loc))
+
+print("\tint move_target[] = { ", end="")
+for i in range( len( offsets)):
+   pre = ", "
+   if ( i == 0):
+      pre = ""
+   print( pre + offsets[ i], end="")
+print("};")
